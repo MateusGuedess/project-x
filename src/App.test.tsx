e10155629c91e1fb32972c5011ae4e83001af1e5
Mateus Guedes <teuguedes@outlook.com>
2021-01-30 01:55:05 -0300

    configure jest and enzyme test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Test App text', () => {
+  it('should be teste inside App div', () => {
+    const container = shallow(<App />)
+
+    expect(container.text()).toBe('teste')
+  })
+})
